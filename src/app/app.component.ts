a10308eede2adabb0968f8066a860a9d9fa51826
yamady0704 <fleis_preis@yahoo.co.jp>
2020-03-04 13:35:06 +0900

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,69 +1,68 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, OnInit, Input } from "@angular/core";
+import { Observable } from "rxjs";
 
-import { GithubApiService } from './services/github-api.service';
-import { Repository } from './types/repository.type';
-import { select_lists } from './lists/select-list'
-import { favorite_lists } from './lists/favorite-list';
-
+import { GithubApiService } from "./services/github-api.service";
+import { Repository } from "./types/repository.type";
+import { select_lists } from "./lists/select-list";
+import { favorite_lists } from "./lists/favorite-list";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-
 export class AppComponent implements OnInit {
-  title = 'app';
+  title = "app";
   @Input() repos$: Observable<Repository[]>;
-  @Input() selected :Repository[] = select_lists 
-  list :Repository[] =new Array();
-  @Input() favorite_list :Repository[] = favorite_lists
-  show = true
-  length: Number = 0
-  loading : boolean = false
-  shown(repository: Repository) :Boolean{
-    for(let i of favorite_lists){
-      if(i.id = repository.id){
-        return false
-      }else{
-        return true
+  @Input() selected: Repository[] = select_lists;
+  list: Repository[] = new Array();
+  @Input() favorite_list: Repository[] = favorite_lists;
+  show = true;
+  length: Number = 0;
+  loading: boolean = false;
+  shown(repository: Repository): Boolean {
+    for (let i of favorite_lists) {
+      if ((i.id = repository.id)) {
+        return false;
+      } else {
+        return true;
       }
     }
   }
-  
+
   constructor(private githubApiService: GithubApiService) {
-    if(localStorage.getItem('json') == null){
-      localStorage.setItem('json', JSON.stringify(this.favorite_list));
+    if (localStorage.getItem("json") == null) {
+      localStorage.setItem("json", JSON.stringify(this.favorite_list));
     }
-    var data = localStorage.getItem('json');
+    var data = localStorage.getItem("json");
     let tmp_list = JSON.parse(data);
-    for(let item of tmp_list){
-      favorite_lists.push(item)
+    for (let item of tmp_list) {
+      favorite_lists.push(item);
     }
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   search(word: string) {
-    this.loading = true
+    this.loading = true;
     console.log(`search: ${word}`);
     this.repos$ = this.githubApiService.searchRepos(word);
-    this.repos$.subscribe(result => {this.length = result.length})
+    this.repos$.subscribe(result => {
+      this.length = result.length;
+    });
   }
 
-  add(){
-    for(let item of select_lists){
-      if(item.order_number == 0 || null){
-        alert("並び順は必ず入力してください")
-        return
+  add() {
+    for (let item of select_lists) {
+      if (item.order_number == 0 || null) {
+        alert("並び順は必ず入力してください");
+        return;
       }
     }
-    if(select_lists.length != 0){
-      select_lists.forEach((k, index) =>{
+    if (select_lists.length != 0) {
+      select_lists.forEach((k, index) => {
         favorite_lists.push(k);
-      })
+      });
       for (var i = select_lists.length - 1; i >= 0; i--) {
         select_lists.splice(i, 1);
       }
@@ -73,29 +72,29 @@ export class AppComponent implements OnInit {
         } else {
           return -1;
         }
-      })
-      this.save()
+      });
+      this.save();
     }
   }
 
-  remove(item: Repository, from: Repository[]){
-    from.forEach((k, index) =>{
-      if(k == item){
-        from.splice(index, 1)
+  remove(item: Repository, from: Repository[]) {
+    from.forEach((k, index) => {
+      if (k == item) {
+        from.splice(index, 1);
       }
-    })
+    });
   }
 
-  delete(item: Repository){
-    this.selected.forEach((k, index) =>{
-      if(k == item){
-        this.selected.splice(index, 1)
+  delete(item: Repository) {
+    this.selected.forEach((k, index) => {
+      if (k == item) {
+        this.selected.splice(index, 1);
       }
-    })
+    });
   }
 
-  private save(){
-    localStorage.setItem('json', JSON.stringify(this.favorite_list));
-    alert("お気に入りリストに追加しました")
+  private save() {
+    localStorage.setItem("json", JSON.stringify(this.favorite_list));
+    alert("お気に入りリストに追加しました");
   }
 }
